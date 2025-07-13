07e5c1e73d752415a8a433c629b2614e98eb507b
Lucàs <86352901+LucasVbr@users.noreply.github.com>
2025-07-14 00:30:34 +0200

    feat: Clear main branch
    Took 3 minutes

--- src/model/social_list.py
+++
@@ -1,8 +0,0 @@
-from src.model.social import Social
-
-
-def social_list(socials: list) -> dict[str, str]:
-    socials: dict[str, str] = {social.get("name"): str(Social(social.get("name"), social.get("url"))) for social in
-                               socials}
-    socials["all"] = "\n".join([str(social) for social in socials.values()])
-    return socials
